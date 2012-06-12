a95e827cf9e229746edb2b9fe88d84317b580a43
Ian Hansen <ian@mediarain.com>
2012-06-11 19:37:07 -0600

    adds simple test case

--- test/index.js
+++ test/index.js
@@ -1,22 +1,36 @@
+"use strict";
+
 var malone = require('../')
-  , karl = malone.createMalone('karl', {redis: {host: 'localhost', port: 6379}})
-  , stockton = malone.createMalone('stockton', {redis: {host: 'localhost', port: 6379}})
+  , options
   ;
 
-karl.ready(function() {
-  stockton.ready(function() {
-    karl.send('stockton', JSON.stringify({test: 'value1'}));
-    karl.send('stockton', JSON.stringify({test: 'value2'}));
-    stockton.send('karl', 'I give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ball');
-    stockton.send('karl', 'I give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ballI give you da ball');
+options = {
+  redis: {
+    host: 'localhost',
+    port: 6379
+  }
+};
+
+describe('test', function() {
+  it('creates a bunch of mailmen at once', function(done) {
+    var m1 = malone.createMalone('m1', options);
+    var m2 = malone.createMalone('m2', options);
+    var m3 = malone.createMalone('m3', options);
+    m2.on('message', function(message) {
+      m2.send('m3', message);
+    });
+    m3.on('message', function(message) {
+      m3.send('m1', message);
+    });
+    m1.on('message', function(message) {
+      done();
+    });
+    m1.ready(function(){
+      m2.ready(function(){
+        m3.ready(function(){
+          m1.send('m2', 'from m1');
+        });
+      });
+    });
   });
 });
-
-karl.on('message', function(message) {
-  console.log('karl malone receive', message);
-});
-
-stockton.on('message', function(message) {
-  console.log('stockton receive', message);
-});
-
